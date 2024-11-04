24e46e58801df3b01e55f7b5cfd440d12ff16b63
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2024-11-04 11:07:50 +0400

    remove unused code

--- src/app/components/ImagesSection.tsx
+++ src/app/components/ImagesSection.tsx
@@ -1,45 +1,6 @@
 import Image from "next/image";
 import React from "react";
 
-interface Images {
-  id: string;
-  src: string;
-  alt: string;
-}
-
-const images: Images[] = [
-  {
-    id: "sdgsd",
-    src: "/images/image1.jpg",
-    alt: "image 1",
-  },
-  {
-    id: "sdgsd",
-    src: "/images/image1.jpg",
-    alt: "image 1",
-  },
-  {
-    id: "sdgsfsdd",
-    src: "/images/image2.jpg",
-    alt: "image 2",
-  },
-  {
-    id: "lzd;gk",
-    src: "/images/image3.jpg",
-    alt: "image 3",
-  },
-  {
-    id: "dsofksd",
-    src: "/images/image4.jpg",
-    alt: "image 4",
-  },
-  {
-    id: "fjkds",
-    src: "/images/image5.jpg",
-    alt: "image 5",
-  },
-];
-
 export const ImagesSection = () => {
   return (
     <div className="flex flex-wrap mt-20 justify-center gap-4">
@@ -56,7 +17,7 @@ export const ImagesSection = () => {
       <div className="w-2/5">
         <Image
           src={"/images/image2.jpg"}
-          alt={"image1"}
+          alt={"image2"}
           layout="responsive"
           width={1920}
           height={1080}
@@ -66,7 +27,7 @@ export const ImagesSection = () => {
       <div className="w-2/5">
         <Image
           src={"/images/image3.jpg"}
-          alt={"image1"}
+          alt={"image3"}
           layout="responsive"
           width={1920}
           height={1080}
@@ -76,7 +37,7 @@ export const ImagesSection = () => {
       <div className="w-1/2">
         <Image
           src={"/images/image4.jpg"}
-          alt={"image1"}
+          alt={"image4"}
           layout="responsive"
           width={1920}
           height={1080}
@@ -86,7 +47,7 @@ export const ImagesSection = () => {
       <div className="w-3/4 max-w-xs">
         <Image
           src={"/images/image5.jpg"}
-          alt={"image1"}
+          alt={"image5"}
           layout="responsive"
           width={1920}
           height={1080}
